288adde1d9095756907b9a4dfe1a3281caf55daa
Matthew T <matthewtindale@hotmail.com>
2024-02-25 18:17:13 +1100

    Implemented Audio
    Implemented audio for helicopter, picking up soldiers and returning soldiers to the base. Also added and implemented audio for the player dying and the player winning.

--- Assets/Scripts/Tree.cs
+++ Assets/Scripts/Tree.cs
@@ -6,16 +6,19 @@ public class Tree : MonoBehaviour
 {
     PlayerController player;
     Canvas deathScreen;
+    AudioSource crashAudio;
     void Start()
     {
         player = FindObjectOfType<PlayerController>();
         deathScreen = GameObject.Find("Death Screen").GetComponent<Canvas>();
+        crashAudio = GameObject.Find("Crash Audio").GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            crashAudio.Play();
             deathScreen.enabled = true;
             Destroy(collision.gameObject);
         }
