b9bcc7f51c24e36750a3bdb1216bf88496243c86
Matthew T <matthewtindale@hotmail.com>
2024-02-25 16:39:28 +1100

    Added the tree
    If the player collides with the tree, then they will be killed.

---
+++ Assets/Scripts/Tree.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    PlayerController player;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
