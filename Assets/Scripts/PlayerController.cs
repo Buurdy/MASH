c3fcb79bea2d2daa66576c9f34c81581b05dbaf8
Matthew T <matthewtindale@hotmail.com>
2024-02-25 19:40:14 +1100

    Screen size fixes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -56,7 +56,7 @@ public class PlayerController : MonoBehaviour
         distanceToCamera = transform.position.z - Camera.main.transform.position.z;
 
         //Sets all the borders of the camera, allows for resizing.
-        leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distanceToCamera)).x + this.transform.localScale.x / 2;
+        leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distanceToCamera)).x - this.transform.localScale.x / 2;
         rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distanceToCamera)).x - this.transform.localScale.x / 2;
         topBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distanceToCamera)).y + this.transform.localScale.y / 2;
         bottomBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, distanceToCamera)).y - this.transform.localScale.y / 2;
